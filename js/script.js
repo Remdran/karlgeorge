721ccfe2ebae7c32a454ba0e34a287d76b88077d
Remdran <karlos287@googlemail.com>
2018-01-23 12:50:52 +0000

    Working on responsiveness

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 $("document").ready(function() {
 
+    $('.navbar-nav>li>a').on('click', function(){
+        $('.navbar-collapse').collapse('hide');
+    });
+
     $(".navbar a").click(function() {
         $(".navbar a").removeClass('active');
         $(this).addClass("active");
